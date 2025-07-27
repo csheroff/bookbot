7b410c75ff633dfd84d6a1362be4cba967890356
Tobitantin <csheroff@comcast.net>
2025-07-27 05:52:09 -0400

    import stats

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
     
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+from stats import get_num_words
 
 main()
